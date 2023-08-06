3685bde67755d84ea972be8e1ad0d9ee37459854
Pola Mounir Samir <polamounir103@gmail.com>
2023-08-07 01:50:32 +0300

    hi

--- js/script.js
+++ js/script.js
@@ -68,6 +68,83 @@ function closeInfoCard(cardItemId , closingId){
     closBtn.style.display = "none "
 }
 
+// }}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+
+function star1(starId1 ,starId2,starId3 , starId4 , starId5) {
+    let star1 = document.querySelector(starId1)
+    let star2 = document.querySelector(starId2)
+    let star3 = document.querySelector(starId3)
+    let star4 = document.querySelector(starId4)
+    let star5 = document.querySelector(starId5)
+    star1.setAttribute ("data-prefix", "fas")
+    star2.setAttribute ("data-prefix", "far")
+    star3.setAttribute ("data-prefix", "far")
+    star4.setAttribute ("data-prefix", "far")
+    star5.setAttribute ("data-prefix", "far")
+    // var active = star1.getAttribute("data-prefix")
+
+}
+function star2(starId1 ,starId2,starId3 , starId4 , starId5) {
+    let star1 = document.querySelector(starId1)
+    let star2 = document.querySelector(starId2)
+    let star3 = document.querySelector(starId3)
+    let star4 = document.querySelector(starId4)
+    let star5 = document.querySelector(starId5)
+    star1.setAttribute ("data-prefix", "fas")
+    star2.setAttribute ("data-prefix", "fas")
+    star3.setAttribute ("data-prefix", "far")
+    star4.setAttribute ("data-prefix", "far")
+    star5.setAttribute ("data-prefix", "far")
+}
+function star3(starId1 ,starId2,starId3 , starId4 , starId5) {
+    let star1 = document.querySelector(starId1)
+    let star2 = document.querySelector(starId2)
+    let star3 = document.querySelector(starId3)
+    let star4 = document.querySelector(starId4)
+    let star5 = document.querySelector(starId5)
+    star1.setAttribute ("data-prefix", "fas")
+    star2.setAttribute ("data-prefix", "fas")
+    star3.setAttribute ("data-prefix", "fas")
+    star4.setAttribute ("data-prefix", "far")
+    star5.setAttribute ("data-prefix", "far")
+}
+function star4(starId1 ,starId2,starId3 , starId4 , starId5) {
+    let star1 = document.querySelector(starId1)
+    let star2 = document.querySelector(starId2)
+    let star3 = document.querySelector(starId3)
+    let star4 = document.querySelector(starId4)
+    let star5 = document.querySelector(starId5)
+    star1.setAttribute ("data-prefix", "fas")
+    star2.setAttribute ("data-prefix", "fas")
+    star3.setAttribute ("data-prefix", "fas")
+    star4.setAttribute ("data-prefix", "fas")
+    star5.setAttribute ("data-prefix", "far")
+}
+function star5(starId1 ,starId2,starId3 , starId4 , starId5) {
+    let star1 = document.querySelector(starId1)
+    let star2 = document.querySelector(starId2)
+    let star3 = document.querySelector(starId3)
+    let star4 = document.querySelector(starId4)
+    let star5 = document.querySelector(starId5)
+    star1.setAttribute ("data-prefix", "fas")
+    star2.setAttribute ("data-prefix", "fas")
+    star3.setAttribute ("data-prefix", "fas")
+    star4.setAttribute ("data-prefix", "fas")
+    star5.setAttribute ("data-prefix", "fas")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 // ||||||||||||||||||||||||||||||||||||||||||||||
 function changCol(boxId , playBtnId , stopBtnId ) {
     var box = document.getElementById(boxId);
